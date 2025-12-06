e25acdfe57efbd2c25ce90a4a77ca4d5c032dcb2
Temirlan <bthero09@gmail.com>
2025-12-06 14:18:21 +0800

    rewrite 06b in a cleaner way

--- archive/06b.zig
+++ archive/06b.zig
@@ -14,51 +14,15 @@ fn readline(delim: u8) ?[]u8 {
     return line;
 }
 
-fn has_digits(line: []u8) bool {
-    for (line) |ch| {
-        if (ch >= '0' and ch <= '9') {
-            return true;
-        }
-    }
-    return false;
-}
-
-fn parse_numbers(line: []u8) ![]i64 {
+fn read_table() ![][]u8 {
     const allocator = std.heap.page_allocator;
-    var nums = try std.ArrayList(i64).initCapacity(allocator, 0);
-    var curr_num: i64 = -1;
+    var table_ct = try std.ArrayList([]u8).initCapacity(allocator, 0);
 
-    for (line) |ch| {
-        if (ch >= '0' and ch <= '9') {
-            if (curr_num == -1) {
-                curr_num = ch - '0';
-            } else {
-                curr_num = curr_num * 10 + (ch - '0');
-            }
-        } else if (curr_num != -1) {
-            try nums.append(allocator, curr_num);
-            curr_num = -1;
-        }
+    while (readline('\n')) |line| {
+        try table_ct.append(allocator, line);
     }
 
-    if (curr_num != -1) {
-        try nums.append(allocator, curr_num);
-    }
-
-    return nums.items;
-}
-
-fn parse_ops(line: []u8) ![]u8 {
-    const allocator = std.heap.page_allocator;
-    var ops = try std.ArrayList(u8).initCapacity(allocator, 0);
-
-    for (line) |ch| {
-        if (ch == '+' or ch == '*') {
-            try ops.append(allocator, ch);
-        }
-    }
-
-    return ops.items;
+    return table_ct.items;
 }
 
 fn calc_expr(nums: []i64, op: u8) i64 {
@@ -79,42 +43,47 @@ fn calc_expr(nums: []i64, op: u8) i64 {
     return res;
 }
 
-pub fn main() !void {
-    const allocator = std.heap.page_allocator;
-    var table_ct = try std.ArrayList([]u8).initCapacity(allocator, 0);
+fn parse_column(col: []u8) struct { ?i64, ?u8 } {
+    var num: ?i64 = null;
+    var op: ?u8 = null;
 
-    while (readline('\n')) |line| {
-        try table_ct.append(allocator, line);
+    for (col) |ch| {
+        if (ch >= '0' and ch <= '9') {
+            if (num == null) {
+                num = ch - '0';
+            } else {
+                num = num.? * 10 + (ch - '0');
+            }
+        } else if (ch == '+' or ch == '*') {
+            op = ch;
+        }
     }
 
-    const table = table_ct.items;
+    return .{ num, op };
+}
+
+pub fn main() !void {
+    const table = try read_table();
     var sum: i64 = 0;
 
+    const allocator = std.heap.page_allocator;
     var nums = try std.ArrayList(i64).initCapacity(allocator, 0);
     var j: usize = table[0].len;
     while (j > 0) {
         j -= 1;
-        var curr_num: i64 = -1;
-        var op: u8 = '?';
+        var col = try allocator.alloc(u8, table.len);
 
         for (0..table.len) |i| {
-            const ch = table[i][j];
-            if (ch >= '0' and ch <= '9') {
-                if (curr_num == -1) {
-                    curr_num = ch - '0';
-                } else {
-                    curr_num = curr_num * 10 + (ch - '0');
-                }
-            } else {
-                if (curr_num != -1) {
-                    try nums.append(allocator, curr_num);
-                    curr_num = -1;
-                }
-                op = ch;
-            }
+            col[i] = table[i][j];
         }
 
-        if (op == '+' or op == '*') {
+        const num_, const op_ = parse_column(col);
+
+        if (num_) |num| {
+            try nums.append(allocator, num);
+        }
+
+        if (op_) |op| {
             sum += calc_expr(nums.items, op);
             nums.clearAndFree(allocator);
         }
